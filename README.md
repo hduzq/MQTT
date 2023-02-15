0c0c3eaa7cb1a97073cc844d48bef850b28b711f
fripSide <clangllvm@126.com>
2023-02-15 10:40:38 +0800

    add jupter notebook

--- README.md
+++ README.md
@@ -1,1 +1,27 @@
-# MQTT
+### MQTT
+
+
+### 开发说明
+### TODO
+1. 熟悉各个云平台的基本功能    
+2. 理解MQTT，为什么用订阅模式，为什么省电，存在什么问题。怎么区分client的身份，能否假冒消息。
+
+#### 项目说明  
+1. 目录结构：  
+```
+$tree -L 1
+.
+├── README.md            
+├── apps                 # 各个云平台的基本功能的完整Demo
+└── cloud-api-demo       # 云平台的Notebook文档和代码段
+```
+
+2. Python版本3.9 or 3.10  
+建议用PyCharm开发，notebook可在浏览器中开发，这里我们使用的jupter lab，为了兼容notebook所以indent都用space(4)。  
+在notebook上开发各平台的API demo，好处是能添加注释以及在文档中保留代码运行结果。  
+```
+pip3 install -r requirements.txt  
+
+jupyter lab
+# 然后在浏览器打开 jupyter的url，例如：http://127.0.0.1:8888/?token=xxx
+```
--- apps/mqtt.py
+++ apps/mqtt.py
@@ -6,7 +6,8 @@ import paho.mqtt.client as mqtt
 MQTTHOST = "iot-06z009y9rnab11u.mqtt.iothub.aliyuncs.com"
 MQTTPORT = 1883
 mqttClient = mqtt.Client(client_id="i6faViFVrIm.light1|securemode=2,signmethod=hmacsha256,timestamp=1676383979937|")
-mqttClient.username_pw_set(username="light1&i6faViFVrIm", password="f0d428467f9fb06467c52f24d11a50491f81c8b4f51684a92682d821ddde29c9")
+mqttClient.username_pw_set(username="light1&i6faViFVrIm",
+                           password="f0d428467f9fb06467c52f24d11a50491f81c8b4f51684a92682d821ddde29c9")
 
 
 # 连接MQTT服务器
@@ -14,13 +15,14 @@ def on_mqtt_connect():
     mqttClient.connect(MQTTHOST, MQTTPORT, 60)
     mqttClient.loop_start()
 
+
 # publish 消息
 def on_publish(topic, payload, qos):
     mqttClient.publish(topic, payload, qos)
 
+
 # 消息处理函数
 def on_message_come(lient, userdata, msg):
-
     print(msg.topic + " " + ":" + str(msg.payload))
 
 
@@ -28,7 +30,7 @@ def on_message_come(lient, userdata, msg):
 def on_subscribe():
     # 订阅监听自定义Topic
     mqttClient.subscribe("light1/user/update1", 1)
-    mqttClient.on_message = on_message_come # 消息到来处理函数
+    mqttClient.on_message = on_message_come  # 消息到来处理函数
 
 
 def main():
@@ -36,10 +38,13 @@ def main():
     # 自定义Topic消息上行
     on_publish("light1/user/test2", "Hello Python!", 1)
     # 系统属性Topic消息上行
-    on_publish("/sys/light1/thing/event/property/post", "{\"method\":\"thing.service.property.set\",\"id\":\"i6faViFVrIm.light1|securemode=2,signmethod=hmacsha256,timestamp=1676381859164|\",\"params\":{\"Status\":1},\"version\":\"1.0.0\"}", 1)
+    on_publish("/sys/light1/thing/event/property/post",
+               "{\"method\":\"thing.service.property.set\",\"id\":\"i6faViFVrIm.light1|securemode=2,signmethod=hmacsha256,timestamp=1676381859164|\",\"params\":{\"Status\":1},\"version\":\"1.0.0\"}",
+               1)
     on_subscribe()
     while True:
         pass
+
 
 if __name__ == '__main__':
     main()
